2711bd63dbc93e29a7cf7e036c8abbe31e88b94c
Vojtěch Kubíček <vojtechkubicek1@post.cz>
2023-08-08 22:55:23 +0200

    update

--- src/components/leaderboard/leaderboard.tsx
+++ src/components/leaderboard/leaderboard.tsx
@@ -1,3 +1,40 @@
+import { collection, getDocs } from "firebase/firestore";
+import { useState, useEffect } from "react";
+import { db } from "../../firebase-config";
+
+type LeaderboardEntry = {
+  id: string;
+  name: string;
+  score: number;
+};
+
 export const Leaderboard = () => {
-  return <div>Leaderboard</div>;
+  const [leaderBoard, setLeaderBoard] = useState<
+    LeaderboardEntry[] | undefined
+  >(undefined);
+
+  const leaderBoardRef = collection(db, "leaderboard");
+  useEffect(() => {
+    const getLeaderBoard = async () => {
+      const data = await getDocs(leaderBoardRef);
+      setLeaderBoard(
+        data.docs.map(
+          (doc) => ({ ...doc.data(), id: doc.id } as LeaderboardEntry)
+        )
+      );
+    };
+
+    getLeaderBoard();
+  }, []);
+
+  return (
+    <div>
+      {leaderBoard?.map((entry) => (
+        <div key={entry.id}>
+          <p>Name: {entry.name}</p>
+          <p>Score: {entry.score}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
--- src/firebase-config.ts
+++ src/firebase-config.ts
@@ -1,7 +1,6 @@
 
 import { initializeApp } from "firebase/app";
 
-import { getAnalytics } from "firebase/analytics";
 
 import {getFirestore} from "firebase/firestore"
 
@@ -26,6 +25,5 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
-const analytics = getAnalytics(app);
 
 export const db = getFirestore();
